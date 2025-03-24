ab52401c871263b83279b02fa2ab935895de391a
LuisGR <lgordillor01@iesalbarregas.es>
2025-03-24 19:55:49 +0100

    Arreglar

--- App.java
+++ App.java
@@ -5,17 +5,10 @@
  */
 public class App{
     public static void main(String[] args) {
-<<<<<<< HEAD
-        //empezamos
-        //seguimos
-=======
         mostrarMenu();
-
-        
     }
     public static void mostrarMenu(){
         System.out.println("----Menú----\n 1- Seleccionar carpeta\n 2- Lectura de fichero \n 3- Conversión a\n 4- Salir");
->>>>>>> Luis
     }
 }
 
