db25d3f52421c245b45d58b01da091242138f250
LuisGR <lgordillor01@iesalbarregas.es>
2025-03-25 16:36:02 +0100

    Pasar el map a cada clase

--- FicheroJson.java
+++ FicheroJson.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class FicheroJson {
-    
+    private static List<Map<String, String>> datos = new ArrayList<>();
 }
