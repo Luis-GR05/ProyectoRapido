7fab45e74db9fbdff6414368f719244b115bc306
LuisGR <lgordillor01@iesalbarregas.es>
2025-03-27 17:25:27 +0100

    Empezar convertir Fichero Json

--- FicheroJson.java
+++ FicheroJson.java
@@ -3,6 +3,9 @@ import java.util.*;
 
 public class FicheroJson {
     public static void convertirFichero(List<LinkedHashMap<String, String>> datos, File archivoSalida) throws IOException {
-        
+        String json = "[\n";
+        json += "]";
+        BufferedWriter bw = new BufferedWriter(new FileWriter(archivoSalida));
+        bw.write(json);
     }
 }
